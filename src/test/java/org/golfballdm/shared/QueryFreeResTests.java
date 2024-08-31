ba7880e1e4e7e7ef9c4d9642a5ec49d153a53565
dmescher <dmescher@gmail.com>
2024-08-30 22:17:08 -0400

    TODO:  * Find out why logging isn't working (no output to console) TODO:  * Write query creation unit tests
    Implemented parameter validation unit tests

--- src/test/java/org/golfballdm/shared/QueryFreeResTests.java
+++ src/test/java/org/golfballdm/shared/QueryFreeResTests.java
@@ -2,9 +2,14 @@ package org.golfballdm.shared;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.*;
+import org.junit.jupiter.api.Test;
 import org.mockito.*;
 import org.golfballdm.models.FreeResident;
 
 
 public class QueryFreeResTests {
+    @Test
+    public void testQueryCreationSingleParameter() {
+
+    }
 }
